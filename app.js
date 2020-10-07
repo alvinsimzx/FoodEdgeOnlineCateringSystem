1e01d7fbe3ba4e7891f5935c3a9bc6463f3c5313
Chan Kwang Yung <khingchan00@gmail.com>
2020-10-07 21:51:21 +0800

    Finished index.html

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 window.addEventListener('scroll', function(){
     const parallax = document.querySelector('.bigBanner');
+    const opacity = document.querySelector('.opacity');
     let scroll = window.pageYOffset;
 
     parallax.style.transform = 'translateY('+ scroll*.3+'px)';
+    opacity.style.opacity =  scroll / (sectionY.top + section_height);
 })
