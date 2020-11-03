29d6b0472ca6f9fea3c1921309f5b807c4f672a0
Chan Kwang Yung <khingchan00@gmail.com>
2020-11-03 18:23:04 +0800

    Fixed some issues with the popup box

--- Scripts/FoodEdgeSystem/accounts/static/js/app.js
+++ Scripts/FoodEdgeSystem/accounts/static/js/app.js
@@ -103,13 +103,10 @@ function editBox(details){
     var deleteConfirm = document.getElementById('deleteYes');
     deleteConfirm.innerHTML = "Edit";
     deleteConfirm.removeAttribute("href");
-    deleteConfirm.setAttribute("onclick", "submitForm()");
+    var id = details.getAttribute("data-form-id")
+    deleteConfirm.setAttribute("onclick", "submitForm(\""+ id+"\")");
 }
 
-function submitForm() {
-    document.getElementById("editForm").submit();
-    document.getElementById("StockEdit").submit();
-    document.getElementById("StockAdd").submit();
-    document.getElementById("StockAdd").submit();
-    document.getElementById("MenuAdd").submit();
+function submitForm(id) {
+    document.getElementById(id).submit();
   }
